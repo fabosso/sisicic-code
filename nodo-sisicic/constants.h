c5a81ff19420fcc843ccc91bd29f20ebbad0eb7c
Franco Abosso <franco.abosso@gmail.com>
2021-04-21 01:01:58 -0300

    Modificaciones en el pinout (p/ interior shelter)

--- nodo-sisicic/constants.h
+++ nodo-sisicic/constants.h
@@ -6,25 +6,18 @@
     @version 1.0 29/03/2021
 */
 
-#define DEBUG_LEVEL 1				// Imprime mensajes por el puerto serial.
+#define DEBUG_LEVEL 5				// Nivel de debug (0 inhabilita el puerto serial).
 #define SERIAL_BPS 9600				// Bitrate de las comunicaciones por puerto serial.
 
 #define LORA_FREQ 433175000 		// Frecuencia de la transmisión LoRa (en Hz).
 #define DEVICE_ID 10009 			// Identificador del nodo.
-#define LORA_TIMEOUT 30			    // Tiempo entre cada mensaje LoRa.
+      
+#define LORA_TIMEOUT 20			    // Tiempo entre cada mensaje LoRa.
 #define LORA_SYNC_WORD 0x34			// Palabra de sincronización LoRa.
 
-#define MAX_DISTANCE 50         	// Distancia al fondo del tanque [F].
-#define MIN_DISTANCE 5          	// Distancia al borde del tanque [B].
-#define CAPACIDAD_COMBUSTIBLE 150   // Capacidad del tanque (en L).
-#define PI_TIMES_R_SQUARED (CAPACIDAD_COMBUSTIBLE) / (MAX_DISTANCE - MIN_DISTANCE)
-#define PING_SAMPLES 5				// Cantidad de muestras ultrasónicos.
+
 
 #define TIMING_SLOTS 3 				// Cantidad de slots necesarios de timing (ver timing_helpers.h)
 
-#define CORRIENTE_MOCK "0.26"       // Valor de sensado falso.
-// #define TENSION_MOCK "223.11"    // Valor de sensado falso.
-// #define FLAME_MOCK "0"           // Valor de sensado falso.
-// #define TEMPERATURA_MOCK "23.11" // Valor de sensado falso.
-// #define LLUVIA_MOCK "0"          // Valor de sensado falso.
-#define NAFTA_MOCK "128.22"         // Valor de sensado falso.
+#define TENSION_MOCK 223.11         // Valor de sensado falso.
+// #define TEMPERATURA_MOCK 23.11   // Valor de sensado falso.
--- nodo-sisicic/pinout.h
+++ nodo-sisicic/pinout.h
@@ -19,33 +19,19 @@
 #define RXTX_PIN -1
 
 // Pinout sensores y actuadores.
-#define CORRIENTE_PIN A2
 #define TENSION_PIN A1
-#define FLAMA_PIN A0
 #define BUZZER_PIN 9
 #define TEMPERATURA_PIN 8
-#define LLUVIA_PIN 7
-#define COMBUSTIBLE_ECHO_PIN 6
-#define COMBUSTIBLE_TRIG_PIN 5
+#define RELE_PIN 7
 
 // Definición de modo de trabajo.
-#define FLAMA_THRESHOLD 2.0
-#define FLAMA_ACTIVA GND
-#if FLAMA_ACTIVA == GND
-    #define FUEGO analogRead(FLAMA_PIN) < (FLAMA_THRESHOLD * 1024 / 3.3)
-#else
-    #define FUEGO analogRead(FLAMA_PIN) >= (FLAMA_THRESHOLD * 1024 / 3.3)
-#endif
 #define BUZZER_ACTIVO LOW
 #define BUZZER_INACTIVO HIGH
-#define LLUVIA_ACTIVA LOW
 
 // Instanciamiento de objetos relacionados al pinout.
 EnergyMonitor eMon;                   
 OneWire oneWireObjeto(TEMPERATURA_PIN);
 DallasTemperature sensorDS18B20(&oneWireObjeto);
-NewPing sonar(COMBUSTIBLE_TRIG_PIN, COMBUSTIBLE_ECHO_PIN, 300);
-
 /**
     setupPinout() determina las I/Os digitales, inicializa al DS18B20
     y calibra los sensores de tensión y corriente.
@@ -53,10 +39,7 @@ NewPing sonar(COMBUSTIBLE_TRIG_PIN, COMBUSTIBLE_ECHO_PIN, 300);
 void setupPinout() {
     pinMode(BUZZER_PIN, OUTPUT);
     pinMode(TEMPERATURA_PIN, INPUT);
-    pinMode(LLUVIA_PIN, INPUT);
-    pinMode(COMBUSTIBLE_ECHO_PIN, INPUT);
-    pinMode(COMBUSTIBLE_TRIG_PIN, OUTPUT);  
+    pinMode(RELE_PIN, OUTPUT);
     sensorDS18B20.begin(); 
     eMon.voltage(TENSION_PIN, 226.0, 1.7);
-    eMon.current(CORRIENTE_PIN, 58.8);      
 }
